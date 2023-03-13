604044e7abcbf4ba7e3823790ea13b741246bbc4
TjayAmit <tristanjayamit0813@gmail.com>
2023-03-13 22:39:30 +0800

    system maintainance update

--- src/component/Routes/CustomRoute.js
+++ src/component/Routes/CustomRoute.js
@@ -49,11 +49,11 @@ const CustomRoute = () => {
   return (
     <Routes location={location} key={location.pathname}>
       <Route path="/" element={<Maintainance />} />
-      <Route element={<RedirectAuth user={user} />}>
+      {/* <Route element={<RedirectAuth user={user} />}>
         <Route path="/" element={<Layout />}></Route>
-      </Route>
+      </Route> */}
       {/* Secure Routes */}
-      <Route element={<ProtectedRoute user={user} />}>
+      {/* <Route element={<ProtectedRoute user={user} />}>
         <Route
           path="/h/*"
           element={
@@ -90,19 +90,19 @@ const CustomRoute = () => {
             </Home>
           }
         />
-      </Route>
+      </Route> */}
 
       {/* public authentication Routes */}
-      <Route element={<ProtectedAuthRoute user={user} />}>
+      {/* <Route element={<ProtectedAuthRoute user={user} />}>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<AdminAuthentication />} />
-      </Route>
+      </Route> */}
 
       {/* catch all */}
-      <Route path="*" element={<PageNotFound />} />
+      {/* <Route path="*" element={<PageNotFound />} />
       <Route path="/credits" element={<Credits />} />
-      <Route path="/recovery" element={<Recovery />} />
+      <Route path="/recovery" element={<Recovery />} /> */}
     </Routes>
   );
 };

c6bd05a800585300f03c5c198ee5ea0da36892ee
TjayAmit <tristanjayamit0813@gmail.com>
2023-03-13 14:18:57 +0800

    maintainance

--- src/component/Routes/CustomRoute.js
+++ src/component/Routes/CustomRoute.js
@@ -16,6 +16,7 @@ import { PatientProvider } from '../dashboard/Pages/Patient SubCollection/Patien
 import Recovery from '../dashboard/Pages/Recovery.js';
 import Loader from '../Loader';
 import ProtectedAuthRoute from './protectedAuthRoute.js';
+import Maintainance from '../dashboard/Pages/Maintainance.js';
 
 const Layout = () => {
   return (
@@ -45,12 +46,13 @@ const CustomRoute = () => {
 
   return (
     <Routes location={location} key={location.pathname}>
-      <Route element={<RedirectAuth user={user} />}>
+      <Route path="/" element={<Maintainance />} />
+      {/* <Route element={<RedirectAuth user={user} />}>
         <Route path="/" element={<Layout />}></Route>
-      </Route>
+      </Route> */}
       {/* Secure Routes */}
       {/* </Route> */}
-      <Route element={<ProtectedRoute user={user} />}>
+      {/* <Route element={<ProtectedRoute user={user} />}>
         <Route
           path="/h/*"
           element={
@@ -87,19 +89,19 @@ const CustomRoute = () => {
             </Home>
           }
         />
-      </Route>
+      </Route> */}
 
       {/* public authentication Routes */}
-      <Route element={<ProtectedAuthRoute user={user} />}>
+      {/* <Route element={<ProtectedAuthRoute user={user} />}>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<AdminAuthentication />} />
-      </Route>
+      </Route> */}
 
       {/* catch all */}
-      <Route path="*" element={<PageNotFound />} />
+      {/* <Route path="*" element={<PageNotFound />} />
       <Route path="/credits" element={<Credits />} />
-      <Route path="/recovery" element={<Recovery />} />
+      <Route path="/recovery" element={<Recovery />} /> */}
     </Routes>
   );
 };
---
+++ src/component/dashboard/Pages/Maintainance.js
@@ -0,0 +1,22 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+import { GiAutoRepair } from 'react-icons/gi';
+import './style.css';
+
+const Maintainance = () => {
+  return (
+    <Box w="100%" h="100vh">
+      <Box className="main" columnGap={5}>
+        <GiAutoRepair size={'20rem'} color="orange" />
+        <Box textAlign={'start'} color="gray">
+          <Text fontSize={30}>Zamboanga City Medical Center Telemedicine</Text>
+          <Text fontSize={100} fontWeight={600} letterSpacing={2}>
+            System Maintainance
+          </Text>
+          <Text fontSize={30}>We will be right back, a year after.</Text>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Maintainance;

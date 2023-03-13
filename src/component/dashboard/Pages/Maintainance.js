f3a998f303fae245d0b02d87a1cd31a559dba17b
TjayAmit <tristanjayamit0813@gmail.com>
2023-03-13 19:50:45 +0800

    maintainance update

--- src/component/dashboard/Pages/Maintainance.js
+++ src/component/dashboard/Pages/Maintainance.js
@@ -12,7 +12,7 @@ const Maintainance = () => {
           <Text fontSize={100} fontWeight={600} letterSpacing={2}>
             System Maintainance
           </Text>
-          <Text fontSize={30}>We will be right back, a year after.</Text>
+          <Text fontSize={30}>Have a time with Family.</Text>
         </Box>
       </Box>
     </Box>
